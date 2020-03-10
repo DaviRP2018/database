e22897411a8f33d3e64ba5549b89b087514ef7ca
Davi Pereira <davirp2009@gmail.com>
2020-03-09 21:11:10 -0300

    detached models from database project

--- core.py
+++ core.py
@@ -5,8 +5,8 @@ import importlib
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import func
 
-from database.models import MyFollowers, MySelf
 from database.settings import DB_STRING
 
 Base = declarative_base()
@@ -18,26 +18,39 @@ class DatabaseSystem():
         """Init the database."""
         self.db = create_engine(DB_STRING)
 
-    def create(self, obj_class):
-        """Create tables in the database."""
-        module = importlib.import_module("database.models")
-        Model = getattr(module, obj_class)
+    def insert(self, Model, data):
+        """Insert rows on a table or create it in the database."""
+        if not data:
+            raise Exception("Data can't be empty or null.")
+
+        # module = importlib.import_module("database.models")
+        # try:
+        #     Model = getattr(module, Model)
+        # except AttributeError:
+        #     raise Exception("There is no such model")
+
+        # if Model == MySelf:
+        #     if self.get(Model):
+        #         raise Exception("There can be only one instagram user.")
 
         Session = sessionmaker(self.db)
         session = Session()
-
         Base.metadata.create_all(self.db)
 
-        # Create
-        model = Model(id=1, link="davi__rp")
+        max_id = session.query(func.max(Model.id))
+        model = Model(id=max_id + 1, **data)
         session.add(model)
         session.commit()
 
-    def read(self):
-        """Read tables."""
+    def get(self, Model):
+        """Get tables."""
+        # module = importlib.import_module("database.models")
+        # Model = getattr(module, Model)
+
         Session = sessionmaker(self.db)
         session = Session()
 
-        tables = session.query(MySelf)
-        for table in tables:
-            print(table.link)
+        tables = session.query(Model)
+        # for table in tables:
+        #     print(table.link)
+        return tables
